0574d4326b7a610e21f6cef3af3f62c0e571a8cc
Adam Jahn <ajjahn@gmail.com>
2016-08-25 23:08:16 -0400

    add schema wrapper

--- opal/prose_mirror/schema.rb
+++ opal/prose_mirror/schema.rb
@@ -1,9 +1,33 @@
+require "native"
+require "prose_mirror/schema/mark_type"
+
 module ProseMirror
   class Schema
     include Native
 
     def self.basic
       new(API.schema_basic)
+    end
+
+    def nodes
+      wrap_native_hash_values(:nodes, NodeType)
+    end
+
+    def marks
+      wrap_native_hash_values(:marks, MarkType)
+    end
+
+    private
+
+    def wrap_native_hash_values(method, klass)
+      hash = {}
+      %x{
+        var target = #{Native.call(@native, method).to_n};
+        Object.keys(target).forEach(function (key) {
+          #{hash[`key`] = klass.new(`target[key]`)}
+        });
+      }
+      hash
     end
   end
 end
--- spec_opal/prose_mirror/schema_spec.rb
+++ spec_opal/prose_mirror/schema_spec.rb
@@ -2,6 +2,20 @@ require "spec_helper"
 
 module ProseMirror
   RSpec.describe Schema do
+    let(:native) { {} }
+    let(:schema) { described_class.new(native.to_n) }
+
+    it "returns the nodes" do
+      native[:nodes] = { doc: {} }
+      expect(schema.nodes).to be_a(Hash)
+      expect(schema.nodes[:doc]).to be_a(NodeType)
+    end
+
+    it "returns the marks" do
+      native[:marks] = { em: {} }
+      expect(schema.marks).to be_a(Hash)
+      expect(schema.marks[:em]).to be_a(MarkType)
+    end
   end
 end
 
