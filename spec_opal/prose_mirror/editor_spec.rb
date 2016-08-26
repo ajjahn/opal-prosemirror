5dda40f120a963a18f274cea484be6ade22ca1d0
Adam Jahn <ajjahn@gmail.com>
2016-08-25 23:22:18 -0400

    remove stubbed specs

--- spec_opal/prose_mirror/editor_spec.rb
+++ spec_opal/prose_mirror/editor_spec.rb
@@ -56,40 +56,6 @@ module ProseMirror
       it " plugins: [Plugin] "
     end
 
-    describe "methods" do
-      it " getOption(name: string) → any "
-      it " setTextSelection(anchor: number, head: ?number = anchor) "
-      it " setNodeSelection(pos: number) "
-      it " setSelection(selection: Selection) "
-      it " setDoc(doc: Node, sel: ?Selection) "
-      it " apply(transform: Transform, options: ?Object = nullOptions) → Transform "
-      it " scrollIntoView: ?bool "
-      it " selection: ?Selection "
-      it " filter: ?bool "
-      it " flush() → bool "
-      it " addKeymap(map: Keymap, priority: ?number = 0) "
-      it " removeKeymap(map: union<string, Keymap>) "
-      it " markRange(from: number, to: number, options: ?Object) → MarkedRange "
-      it " inclusiveLeft: bool = false "
-      it " inclusiveRight: bool = false "
-      it " removeWhenEmpty: bool = true "
-      it " className: string "
-      it " onRemove: fn(number, number) "
-      it " removeRange(range: MarkedRange) "
-      it " activeMarks() → [Mark] "
-      it " addActiveMark(mark: Mark) "
-      it " removeActiveMark(markType: MarkType) "
-      it " focus() "
-      it " hasFocus() → bool "
-      it " posAtCoords(coords: {top: number, left: number}) → ?number "
-      " contextAtCoords(coords: {top: number, left: number}) → ?{pos: number, inside:
-it [{pos: number, node: Node}]} "
-      it " coordsAtPos(pos: number) → {top: number, left: number, bottom: number} "
-      it " scrollIntoView(pos: ?number = null) "
-      it " translate(string: string) → string "
-      it " scheduleDOMUpdate(f: fn() → ?fn() → ?fn()) "
-      it " unscheduleDOMUpdate(f: fn() → ?fn() → ?fn()) "
-      it " updateScheduler(subscriptions: [Subscription], start: fn() → ?fn()) → UpdateScheduler "
     def stub_native(native, method, value)
       `#{ native }[#{ method }] = #{ -> { value } }`
       native
