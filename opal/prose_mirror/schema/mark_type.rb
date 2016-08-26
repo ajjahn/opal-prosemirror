da1904f9764ac177c862c045e0615e626e078a28
Adam Jahn <ajjahn@gmail.com>
2016-08-26 00:39:02 -0400

    opal 0.7.x doesn’t like alias_native ending in ?

--- opal/prose_mirror/schema/mark_type.rb
+++ opal/prose_mirror/schema/mark_type.rb
@@ -5,6 +5,7 @@ module ProseMirror
     include Native
 
     alias_native :instance, :instance, as: Mark
-    alias_native :in_set?, :isInSet, as: Mark
+    alias_native :in_set, :isInSet, as: Mark
+    alias_method :in_set?, :in_set
   end
 end
