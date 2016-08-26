9c5925b0b64f38ec6d3839836eea8e0e6572cfb1
Adam Jahn <ajjahn@gmail.com>
2016-08-25 23:11:16 -0400

    add in set? to mark

--- opal/prose_mirror/mark.rb
+++ opal/prose_mirror/mark.rb
@@ -1,5 +1,7 @@
 module ProseMirror
   class Mark
     include Native
+
+    alias_native :in_set?, :isInSet
   end
 end
--- spec_opal/prose_mirror/mark_spec.rb
+++ spec_opal/prose_mirror/mark_spec.rb
@@ -1,4 +1,19 @@
 module ProseMirror
   RSpec.describe Mark do
+    let(:native) { {} }
+    let(:object) { described_class.new(native.to_n) }
+
+    describe "#in_set?" do
+      it "passes argument to native isInSet" do
+        result = "xyz"
+        native[:isInSet] = -> (arg) { result = arg.first; `{}` }
+        object.in_set?([ Native("argument") ])
+
+        expect(result).to eq("argument")
+      end
+
+      it "delegates to native isInSet" do
+      end
+    end
   end
 end
