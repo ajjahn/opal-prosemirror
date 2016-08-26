1cd3e2dad18dcfb40450016544af9ffcca0ea04d
Adam Jahn <ajjahn@gmail.com>
2016-08-25 23:08:45 -0400

    add native aliases to node type

--- opal/prose_mirror/node_type.rb
+++ opal/prose_mirror/node_type.rb
@@ -1,5 +1,13 @@
 module ProseMirror
   class NodeType
     include Native
+
+    alias_native :block?, :isBlock
+    alias_native :text_block?, :isTextblock
+    alias_native :inline?, :isInline
+    alias_native :text?, :isText
+    alias_native :leaf?, :isLeaf
+    alias_native :selectable?, :selectable
+    alias_native :draggable?, :draggable
   end
 end
--- spec_opal/prose_mirror/node_type_spec.rb
+++ spec_opal/prose_mirror/node_type_spec.rb
@@ -1,5 +1,22 @@
 module ProseMirror
   RSpec.describe NodeType do
+    let(:native) { {} }
+    let(:object) { described_class.new(native.to_n) }
+
+    {
+      block?: :isBlock,
+      text_block?: :isTextblock,
+      inline?: :isInline,
+      text?: :isText,
+      leaf?: :isLeaf,
+      selectable?: :selectable,
+      draggable?: :draggable,
+    }.each do |wrapper_method, native_method|
+      it "delegates ##{wrapper_method} to native #{native_method} method" do
+        native[native_method] = -> { "xyz" }
+        expect(object.public_send(wrapper_method)).to eq("xyz")
+      end
+    end
   end
 end
 
